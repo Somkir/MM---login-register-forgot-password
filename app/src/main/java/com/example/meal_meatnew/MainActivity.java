9bcbf3a554228d2cc110d7c0e380ea407e02652c
anjil <anjilpandit71@gmail.com>
2025-03-05 10:13:56 +0545

    2. Completed splacescreen with fragements

---
+++ app/src/main/java/com/example/meal_meatnew/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.meal_meatnew;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+    private TextView userEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        // Set up Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Set up Navigation Drawer
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Set user's email in the Navigation Drawer header
+        userEmail = navigationView.getHeaderView(0).findViewById(R.id.user_details);
+        if (user != null) {
+            userEmail.setText(user.getEmail());
+        } else {
+            // Redirect to Login if user is not logged in
+            startActivity(new Intent(this, Login.class));
+            finish();
+        }
+
+        // Load the default Home Fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new HomeFragment())
+                    .commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new HomeFragment())
+                    .commit();
+        } else if (id == R.id.nav_about) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new AboutFragment())
+                    .commit();
+        } else if (id == R.id.nav_weeks) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new WeeksFragment())
+                    .commit();
+        } else if (id == R.id.nav_logout) {
+            auth.signOut();  // Logout user
+            startActivity(new Intent(this, Login.class)); // Redirect to Login
+            finish(); // Close MainActivity
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
